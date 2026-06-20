0e27406006b122544c7f5175bba1ad012dd6cea7
vishnugitit <vishnujangam1527@gmail.com>
2026-06-20 14:08:18 +0530

    email test

--- server/server.js
+++ server/server.js
@@ -2,47 +2,52 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const nodemailer = require("nodemailer");
+// ADD THIS HERE
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+const orderRoutes = require("./routes/orderRoutes");
 
-const orderRoutes =
-require("./routes/orderRoutes");
+const adminRoutes = require("./routes/adminRoutes");
 
-const adminRoutes =
-require("./routes/adminRoutes");
+const contactRoutes = require("./routes/contactRoutes");
 
-const contactRoutes =
-require("./routes/contactRoutes");
-
-const {
-  connectWhatsApp,
-} = require("./utils/whatsapp");
+const { connectWhatsApp } = require("./utils/whatsapp");
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
 
-app.use(
-  "/api/orders",
-  orderRoutes
-);
+app.use("/api/orders", orderRoutes);
 
-app.use(
-  "/api/admin",
-  adminRoutes
-);
-app.use(
-  "/api/contact",
-  contactRoutes
-);
+app.use("/api/admin", adminRoutes);
+app.use("/api/contact", contactRoutes);
 connectWhatsApp();
-app.listen(
-  process.env.PORT || 5000,
-  () => {
-    console.log(
-      `Server Running on ${process.env.PORT}`
-    );
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server Running on ${process.env.PORT}`);
+});
+// TEST ROUTE
+app.get("/test-mail", async (req, res) => {
+  try {
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: "jangamvishnuvardhan@gmail.com",
+      subject: "Mail Test",
+      text: "Mail working successfully",
+    });
+
+    res.send("Mail Sent Successfully");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err.message);
   }
-);
+});
 const db = require("./config/db");
 
 app.get("/test-db", async (req, res) => {
@@ -52,23 +57,7 @@ app.get("/test-db", async (req, res) => {
   } catch (err) {
     res.status(500).json({
       error: err.message,
-      code: err.code
+      code: err.code,
     });
-  }
-});
-
-app.get("/test-mail", async (req, res) => {
-  try {
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: "jangamvishnuvardhan@gmail.com",
-      subject: "Mail Test",
-      text: "Mail working successfully"
-    });
-
-    res.send("Mail Sent");
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err.message);
   }
 });
