19c046830c48b44e64b5c2cebc99e09b6101fa88
vishnugitit <vishnujangam1527@gmail.com>
2026-06-20 14:39:56 +0530

    test4

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ console.log("EMAIL_USER:", process.env.EMAIL_USER);
 console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
 
 // SMTP Transport
-const nodemailer = require("nodemailer");
+
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
