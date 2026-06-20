6d874215b4211085fec75d9737dd57a60f86e539
vishnugitit <vishnujangam1527@gmail.com>
2026-06-20 10:53:45 +0530

    Fix Netlify redirects

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "http://localhost:5000/api",
+    "https://popcorn-palace-cgs9.onrender.com/api",
 });
 
 api.interceptors.request.use(
