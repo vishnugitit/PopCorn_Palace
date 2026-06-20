f00a1070ae7c3a04410ecbbce5eb6857d2c13d3c
vishnugitit <vishnujangam1527@gmail.com>
2026-06-20 14:37:49 +0530

    user fix

--- server/server.js
+++ server/server.js
@@ -18,14 +18,21 @@ console.log("EMAIL_USER:", process.env.EMAIL_USER);
 console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
 
 // SMTP Transport
+const nodemailer = require("nodemailer");
+
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  family: 4,
 });
 
 // Verify SMTP on startup
